d3bbb44a034c79d13d9363095c9617879d491075
Смирнова Полина <podmsmirnova@edu.hse.ru>
2026-06-03 03:53:36 +0300

    в class Ingredient был реализован один метод, написан сеттер и геттер для атрибута quantity

--- src/ingredient.py
+++ src/ingredient.py
@@ -1,2 +1,22 @@
+class Ingredient:
+    def __init__(self, name: str, quantity: float, unit: str):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+    @property
+    def quantity(self):
+        return self._quantity
+    @quantity.setter
+    def quantity(self, x):
+        if x <= 0:
+            raise ValueError("Количество должно быть положительным")
+        if x > 0:
+            x = float(x)
+            self._quantity = x
+
+    
+
+
+
 
 
