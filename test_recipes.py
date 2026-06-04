f9dc493914f790eeb8dcc7516d375d664f5ebe66
Смирнова Полина <podmsmirnova@edu.hse.ru>
2026-06-04 03:22:36 +0300

    прописаны все тесты для класса ShoppingList. Все тесты работают корректно

--- test_recipes.py
+++ test_recipes.py
@@ -126,3 +126,101 @@ def test_shoppingListRemove():
     shopList.remove_recipe("тесто")
     listOfShops = shopList.get_list()
     assert len(listOfShops) == 0
+def test_shoppingListRemoveNothing():
+    ingr = []
+    ingr1 = Ingredient("мука", 500, "г")
+    ingr.append(ingr1)
+    ingr2 = Ingredient("вода", 1, "л")
+    ingr.append(ingr2)
+    recipe = Recipe("тесто", ingr)
+    shopList = ShoppingList()
+    shopList.add_recipe(recipe, 1.0)
+    shopList.remove_recipe("пицца")
+    listOfShops = shopList.get_list()
+    assert len(listOfShops) == 2
+def test_shoppingListGetListSameIngredients():
+    ingr = []
+    ingr1 = Ingredient("мука", 500, "г")
+    ingr.append(ingr1)
+    ingr2 = Ingredient("вода", 1, "л")
+    ingr.append(ingr2)
+    recipe = Recipe("тесто", ingr)
+    shopList = ShoppingList()
+    shopList.add_recipe(recipe, 1.0)
+    ingrr = []
+    ingr11 = Ingredient("мука", 600, "г")
+    ingrr.append(ingr11)
+    ingr12 = Ingredient("помидоры", 300, "г")
+    ingrr.append(ingr12)
+    recipe2 = Recipe("пицца", ingrr)
+    shopList.add_recipe(recipe2, 1.0)
+    listOfShops = shopList.get_list()
+    assert len(listOfShops) == 3
+    assert listOfShops[1].quantity == 1100
+    assert listOfShops[0].name == "вода"
+    assert listOfShops[1].name == "мука"
+    assert listOfShops[2].name == "помидоры"
+def test_shoppingListAddTwoLists():
+    ingr = []
+    ingr1 = Ingredient("мука", 500, "г")
+    ingr.append(ingr1)
+    ingr2 = Ingredient("вода", 1, "л")
+    ingr.append(ingr2)
+    recipe = Recipe("тесто", ingr)
+    shopList1 = ShoppingList()
+    shopList1.add_recipe(recipe, 1.0)
+    list1 = shopList1.get_list()
+    ingrr = []
+    ingr11 = Ingredient("мука", 600, "г")
+    ingrr.append(ingr11)
+    ingr12 = Ingredient("помидоры", 300, "г")
+    ingrr.append(ingr12)
+    recipe2 = Recipe("пицца", ingrr)
+    shopList2 = ShoppingList()
+    shopList2.add_recipe(recipe2, 1.0)
+    list2 = shopList2.get_list()
+    new = shopList1 + shopList2
+    listOfShops = new.get_list()
+    assert len(listOfShops) == 3
+    assert listOfShops[0].name == "вода"
+    assert listOfShops[1].name == "мука"
+    assert listOfShops[2].name == "помидоры"
+    assert listOfShops[0].quantity == 1.0
+    assert listOfShops[1].quantity == 1100.0
+    assert listOfShops[2].quantity == 300.0
+def test_shoppingListAddOldLists():
+    ingr = []
+    ingr1 = Ingredient("мука", 500, "г")
+    ingr.append(ingr1)
+    ingr2 = Ingredient("вода", 1, "л")
+    ingr.append(ingr2)
+    recipe = Recipe("тесто", ingr)
+    shopList1 = ShoppingList()
+    shopList1.add_recipe(recipe, 1.0)
+    list1 = shopList1.get_list()
+    ingrr = []
+    ingr11 = Ingredient("мука", 600, "г")
+    ingrr.append(ingr11)
+    ingr12 = Ingredient("помидоры", 300, "г")
+    ingrr.append(ingr12)
+    recipe2 = Recipe("пицца", ingrr)
+    shopList2 = ShoppingList()
+    shopList2.add_recipe(recipe2, 1.0)
+    list2 = shopList2.get_list()
+    new = shopList1 + shopList2
+    listOfShops = new.get_list()
+    assert len(list1) == 2
+    assert len(list2) == 2
+    assert list1[0].name == "вода"
+    assert list1[1].name == "мука"
+    assert list1[0].quantity == 1.0
+    assert list1[1].quantity == 500.0
+    assert list2[0].name == "мука"
+    assert list2[1].name == "помидоры"
+    assert list2[0].quantity == 600,0
+    assert list2[1].quantity == 300.0
+
+
+
+    
+
